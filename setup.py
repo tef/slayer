6267c3eb78f61c7b68eda8653caf5711ac304e2b
tef <tef@twentygototen.org>
2012-06-14 23:38:37 +0100

    tests, travis, setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import glob 
+
+from setuptools import setup
+
+setup(name='slayer',
+      version='0.0.0.0.0.0.0.0',
+      license="MIT License",
+      description='a python parsing library',
+      author='tef',
+      author_email='tef@twentygototen.org',
+      modules=['slayer'],
+      test_suite = "tests",
+     )
+
